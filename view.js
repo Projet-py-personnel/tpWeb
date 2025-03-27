17e449218f9e28f0f5f8e49b7d1a10d7a76073c7
N'Deko Aline <aline.ndeko@etudiant.univ-rennes.fr>
2025-03-27 11:36:29 +0100

    Commit de la classe view.js

--- view.js
+++ view.js
@@ -1,5 +1,24 @@
 
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
 
+Rectangle.prototype.paint = function(ctx){
+    ctx.strokeStyle = this.color
+    ctx.lineWidth = this.thickness
+    ctx.strokeRect(this.startX, this.startY, this.width, this.height)
+};
 
+Line.prototype.paint = function(ctx){
+   ctx.strokeStyle = this.color
+   ctx.lineWidth = this.thickness
+   ctx.beginPath();
+   ctx.moveTo(this.startX , this.startY)
+   ctx.lineTo(this.endX , this.endY)
+   ctx.stroke();
+}
 
+Drawing.prototype.paint = function(ctx , canvas){
+    ctx.fillStyle = '#F0F0F0'
+
+    ctx.fillRect(0 , 0 , canvas.with , canvas.height)
+    this.ShapeArray.ForEach(element => element.paint(ctx))
+}
